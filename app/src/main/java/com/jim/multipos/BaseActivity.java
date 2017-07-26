c33b1d6ef8503cbfc85c32aaab5d81dc4df7d22f
provizor54 <hpshka@mail.ru>
2017-07-26 19:56:01 +0500

    Создал BaseActivity, FirstConfigureActivity, activity_base.xml, activity_first_configure. Добавил строковые ресурсы в string.xml и string.xml (ru) перевел ресурсы на русский язык. AppTheme добавил скрытие Toolbar и Title в Toolbar. BaseAcivity добавлен метод hideStatusBar для скрытие статус бара.

---
+++ app/src/main/java/com/jim/multipos/BaseActivity.java
@@ -0,0 +1,23 @@
+package com.jim.multipos;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+/**
+ * Created by user on 26.07.17.
+ */
+
+public class BaseActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        hideStatusBar();
+    }
+
+    public void hideStatusBar() {
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN);
+    }
+}
---
+++ app/src/main/java/com/jim/multipos/FirstConfigure.java
@@ -0,0 +1,22 @@
+package com.jim.multipos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.FrameLayout;
+
+public class FirstConfigure extends BaseActivity {
+    private FrameLayout leftContainer;
+    private FrameLayout rightContainer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first_configure);
+
+        leftContainer = (FrameLayout) findViewById(R.id.leftContainer);
+        rightContainer = (FrameLayout) findViewById(R.id.rightContainer);
+    }
+}
